a2d399c42976a17191032643602557e3036d65cf
Vision295 <theophilewacthel@gmail.com>
2025-02-05 08:33:09 +0100

    basic inputs

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+
+let wordToGuess = "Friend"
+
+
+console.log("How many players ?")
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+
+console.log(
+      `Let the game JUST ONE begin !!!! \n\t Active player : stop looking at the screen !
+      The ${readline} other players you'll have to make him guess the word ... ${wordToGuess}`
+)
+
+
 console.log("Hello World")
