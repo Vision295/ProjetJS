c515494fccbf190f1fa8d5ab81d71b0c500c9a92
Vision295 <theophilewacthel@gmail.com>
2025-02-05 09:55:57 +0100

    v1 fonctionnelle (ne détecte pas les duplicates)

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
-let wordToGuess = "Friend"
+let wordToGuess = "Friend";
 
 const prompt = require("prompt-sync")({ sigint: true });
 
-let nbPlayer = "haha"
+let nbPlayer = "haha";
 do {
       nbPlayer = prompt("How many players ?\t");
       console.log(`There are : ${nbPlayer} players. \n`);
@@ -13,16 +13,42 @@ do {
 getactivePlayer = function(min, max) {
       return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-let activePlayer = getactivePlayer(1, nbPlayer)
+let activePlayer = getactivePlayer(1, nbPlayer);
 
 function sleep(ms) {
       return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+let clues = [];
+let currentGuess = "haha";
+
+
+function filterClues(clues) {
+      return clues
+}
+
+
 async function run() {
+      console.log(`Let the game JUST ONE begin !!!! \n\t Active player (Player number : ${activePlayer}): stop looking at the screen !`);
       await sleep(2000);
-      console.log(`Let the game JUST ONE begin !!!! \n\t Active player (Player number : ${activePlayer}): stop looking at the screen !`)
-      console.log("")
+      console.log("Everyone has to give one by one their clues to make the active player guess ... The word to guess is : ", wordToGuess);
+      for (let i = 1; i < nbPlayer; i++) {
+            currentGuess = prompt(`Player ${i} give your clue : \t`);
+            clues.push(currentGuess);
+      }
+      console.log("Everyone has given their clues the active player can come back : ", nbPlayer);
+
+      for (let clue of clues) {
+            console.log(clue);
+      }
+      
+      console.log("Try to guess now : ");
+      let guess = prompt(`Active plyaer (Player number : ${activePlayer} try to guess the word : \t`);
+      if (guess == wordToGuess) {
+            console.log("You have won well done !!! ");
+      } else {
+            console.log("You have lost :( \nThe word you had to guess was : ", wordToGuess)
+      }
 }
 
 
