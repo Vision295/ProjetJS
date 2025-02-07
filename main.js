f05c201628ad9e6af6f2bf2fe4f358e06d35ae04
Vision295 <theophilewacthel@gmail.com>
2025-02-07 13:56:48 +0100

    no udplicates no comments

--- main.js
+++ main.js
@@ -41,6 +41,17 @@ function sleep(ms) {
       return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function removeDuplicates(arr) {
+      let seen = new Set();
+      return arr.filter(item => {
+          if (seen.has(item)) {
+              return false;
+          }
+          seen.add(item);
+          return true;
+      });
+  }
+
 async function run() {
       try {
             let nbPlayer = await getInput("Enter the number of player : ", true);
@@ -59,6 +70,7 @@ async function run() {
                   }
             }
             
+            realclues = removeDuplicates(clues)
             await sleep(2000)
             console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 
@@ -81,53 +93,5 @@ async function run() {
       }
 }
 
-/*
-const prompt = require("prompt-sync")({ sigint: true });
-
-let nbPlayer = "haha";
-do {
-      nbPlayer = prompt("How many players ?\t");
-      console.log(`There are : ${nbPlayer} players. \n`);
-} while (!Number.isInteger(parseInt(nbPlayer))) 
-
-
-let activePlayer = getactivePlayer(1, nbPlayer);
-
-function sleep(ms) {
-      return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-let clues = [];
-let currentGuess = "haha";
-
-
-function filterClues(clues) {
-      return clues
-}
-
-
-async function run() {
-      console.log(`Let the game JUST ONE begin !!!! \n\t Active player (Player number : ${activePlayer}): stop looking at the screen !`);
-      await sleep(2000);
-      console.log("Everyone has to give one by one their clues to make the active player guess ... The word to guess is : ", wordToGuess);
-      for (let i = 1; i < nbPlayer; i++) {
-            currentGuess = prompt(`Player ${i} give your clue : \t`);
-            clues.push(currentGuess);
-      }
-      console.log("Everyone has given their clues the active player can come back : ", nbPlayer);
-
-      for (let clue of clues) {
-            console.log(clue);
-      }
-      
-      console.log("Try to guess now : ");
-      let guess = prompt(`Active plyaer (Player number : ${activePlayer} try to guess the word : \t`);
-      if (guess == wordToGuess) {
-            console.log("You have won well done !!! ");
-      } else {
-            console.log("You have lost :( \nThe word you had to guess was : ", wordToGuess)
-      }
-}
-*/
 
 run()
